47a91a20b2226c3770313aa85637e758953d5661
Rafliansyah <rafli.ansyahh@mhs.mdp.ac.id>
2023-01-04 00:43:31 +0700

    Update MainActivity.java

--- app/src/main/java/com/si5a/findhighschool/MainActivity.java
+++ app/src/main/java/com/si5a/findhighschool/MainActivity.java
@@ -1,9 +1,11 @@
 package com.si5a.findhighschool;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,8 +17,18 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_about,menu);
-        return super.onCreateOptionsMenu(menu);
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.menu_card:
+                tampilDataCard();
+                break;
+            case R.id.menu_grid:
+                tampilDataGrid();
+                break;
+            case R.id.menu_about:
+                tampilDataAbout;
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
